8177a1addce2639fab3e67b8cf9bb3c136eb28e7
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2026-02-10 11:18:39 +0200

    ref : and Header with it css from the app

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+
+
+
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+        <header>
+            <img src="../assets/logo.png" alt="logo" />
+        <h1>my travel journal.</h1>
+        </header>
+    
+    </>
+  )
+}
+
+export default Header
