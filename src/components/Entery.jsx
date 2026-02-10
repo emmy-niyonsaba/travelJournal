cb221fbf8f167b49f18fd15893920c2a8d93225b
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2026-02-10 16:22:18 +0200

    done with implementing the props to finish travelproject

--- src/components/Entery.jsx
+++ src/components/Entery.jsx
@@ -5,17 +5,17 @@
 
 import React from 'react'
 
-import marker from '../assets/marker.png'
 
-function Entery() {
+
+function Entery({imageUrl,marker, country, googleMapsUrl, title, startDate, endDate, description}) {
   return (
 
      <article className="journal-entry">
             <div className="main-image-container">
                 <img 
                     className="main-image"
-                    src="https://scrimba.com/links/travel-journal-japan-image-url" 
-                    alt="mount fuji" 
+                    src={imageUrl} 
+                    alt={title} 
                 />
             </div>
             <div className="info-container">
@@ -24,11 +24,11 @@ function Entery() {
                     src={marker} 
                     alt="map marker icon"
                 />
-                <span className="country">Japan</span>
-                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google Maps</a>
-                <h2 className="entry-title">Mount Fuji</h2>
-                <p className="trip-dates">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="entry-text">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <span className="country">{country}</span>
+                <a href={googleMapsUrl}>View on Google Maps</a>
+                <h2 className="entry-title">{title}</h2>
+                <p className="trip-dates">{startDate} - {endDate}</p>
+                <p className="entry-text">{description}</p>
             </div>
             
         </article>
